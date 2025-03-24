0fb203b44e3bba1b9c860576cce2fd6e52bd509d
TranHenry1105 <91947700+quocandeptai11052003@users.noreply.github.com>
2025-03-25 02:57:06 +0700

    Add files via upload

---
+++ tang-crush-main/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Huỳnh Như ơi !',
+    introDesc: `Sao em ngoo quá e lại buồn vì thứ tình cảm không đáng trong khi Anh An là người yêu em, enm là ngoại lệ của anh em biết k=_)))`,
+    btnIntro: 'Click vào đây nè bbi ngoan xinh eu cua anh <3',
+    title: 'Nói thẳng ra anh An đã single gất gất lâu, ừ thì ở hiện tại Anh chọn bé anh như v ó bbi cụa anh',
+    desc: 'nói chung là Quốc An yêu cô cũng cũng đó mà cổ khờ quá =)) nói chung còn nhỏ nên chưa hiểu nhiều lắm =))',
+    btnYes: '=)) nói chung hoi sến nhưng em yêu anh nha =((((',
+    btnNo: 'êu thằng cha nào đó làm em không vui <3',
+    question: 'Trên thế giới hơn 7 tỉ người 6 tỉ 9 thằng tốt đẹp trai sao iu anh dị bé =)))))',
+    btnReply: 'Gửi cho Quốc AN',
+    reply: 'Hôm nào đi chơi với anh nha <3',
+    mess: 'nói chung là anh yêu em nha😘😘',
+    messDesc: 'Tối mai 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
